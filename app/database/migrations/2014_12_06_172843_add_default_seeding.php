94477e7d95012e96fc1ebeb58c2dbfd73da26e4a
Robby Fussell <robbyfussell@fas.harvard.edu>
2014-12-06 13:15:43 -0500

    Modified seeding

--- app/database/migrations/2014_12_06_172843_add_default_seeding.php
+++ app/database/migrations/2014_12_06_172843_add_default_seeding.php
@@ -16,32 +16,13 @@ class AddDefaultSeeding extends Migration {
 	 */
 	public function up()
 	{
-		/*********************************************************/
-		// Create new Permissions
-		/*********************************************************/
+		/*****************************************************************/
+		//Create and assign for CSC Admin
+		/*****************************************************************/
 		$permission = new Toddish\Verify\Models\Permission;
 		$permission->name = 'admin';
 		$permission->description = 'Everything';
 		$permission->save();
-
-		$permission = new Toddish\Verify\Models\Permission;
-		$permission->name = 'read_only';
-		$permission->description = 'Read Only Permission';
-		$permission->save();
-
-		$permission = new Toddish\Verify\Models\Permission;
-		$permission->name = 'employee';
-		$permission->description = 'Permission for employee role';
-		$permission->save();
-
-		/*********************************************************/		
-		// Create a new Roles
-		/*********************************************************/
-		$role = new Toddish\Verify\Models\Role;
-		$role->name = 'CSC_client';
-		$role->description = 'CSC client role with read only permission';
-		$role->level = 1;
-		$role->save();
 
 		$role = new Toddish\Verify\Models\Role;
 		$role->name = 'CSC_Admin';
@@ -49,44 +30,7 @@ class AddDefaultSeeding extends Migration {
 		$role->level = 5;
 		$role->save();
 
-		$role = new Toddish\Verify\Models\Role;
-		$role->name = 'CSC_Employee';
-		$role->description = 'CSC employee role with RTM permissions';
-		$role->level = 3;
-		$role->save();
-
-		/*********************************************************/
-		//Assign permissions to role
-		/*********************************************************/
-		$role = Role::find(2);
-		$role->permissions()->sync(array(1));
-
-		$role = Role::find(1);
-		$role->permissions()->sync(array(2));
-
-		$role = Role::find(3);
-		$role->permissions()->sync(array(3));
-
-		/*********************************************************/
-		// Create default users and assign roles
-		/*********************************************************/
-		$user = new Toddish\Verify\Models\User;
-		$user->username = 'client';
-		$user->email = 'client@xyz.com';
-		$user->password = 'client123'; // This is automatically salted and encrypted
-		$user->verified = 1;
-		$user->save();
-
-		$user->roles()->sync(array(1));
-
-		$user = new Toddish\Verify\Models\User;
-		$user->username = 'employee';
-		$user->email = 'employee@xyz.com';
-		$user->password = 'employee123'; // This is automatically salted and encrypted
-		$user->verified =1;
-		$user->save();
-
-		$user->roles()->sync(array(3));
+		$role->permissions()->sync(array($permission->id));
 
 		$user = new Toddish\Verify\Models\User;
 		$user->username = 'admin';
@@ -95,7 +39,59 @@ class AddDefaultSeeding extends Migration {
 		$user->verified =1;
 		$user->save();
 
-		$user->roles()->sync(array(2));
+		$user->roles()->sync(array($role->id));
+
+		/*****************************************************************/
+		//Create and assign for CSC Employee
+		/*****************************************************************/
+		$permission = new Toddish\Verify\Models\Permission;
+		$permission->name = 'employee';
+		$permission->description = 'Permission for employee role';
+		$permission->save();
+
+		$role = new Toddish\Verify\Models\Role;
+		$role->name = 'CSC_Employee';
+		$role->description = 'CSC employee role with RTM permissions';
+		$role->level = 3;
+		$role->save();
+
+		$role->permissions()->sync(array($permission->id));
+
+		$user = new Toddish\Verify\Models\User;
+		$user->username = 'employee';
+		$user->email = 'employee@xyz.com';
+		$user->password = 'employee123'; // This is automatically salted and encrypted
+		$user->verified =1;
+		$user->save();
+
+		$user->roles()->sync(array($role->id));
+
+
+		/*****************************************************************/
+		//Create and assign for CSC Client
+		/*****************************************************************/
+		$permission = new Toddish\Verify\Models\Permission;
+		$permission->name = 'read_only';
+		$permission->description = 'Read Only Permission';
+		$permission->save();
+
+		$role = new Toddish\Verify\Models\Role;
+		$role->name = 'CSC_client';
+		$role->description = 'CSC client role with read only permission';
+		$role->level = 1;
+		$role->save();
+
+		$role->permissions()->sync(array($permission->id));
+
+		$user = new Toddish\Verify\Models\User;
+		$user->username = 'client';
+		$user->email = 'client@xyz.com';
+		$user->password = 'client123'; // This is automatically salted and encrypted
+		$user->verified = 1;
+		$user->save();
+
+		$user->roles()->sync(array($role->id));
+	
 	}
 
 	/**
