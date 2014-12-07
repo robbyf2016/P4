bc31d45332d6270653e778840cd5ad90b6c4bb73
Robby Fussell <robbyfussell@fas.harvard.edu>
2014-12-07 18:57:04 -0500

    Added landing page application instructions.

--- app/config/session.php
+++ app/config/session.php
@@ -29,7 +29,7 @@ return array(
 	|
 	*/
 
-	'lifetime' => 5,
+	'lifetime' => 0,
 
 	'expire_on_close' => true,
 
--- app/views/CSC_landing.blade.php
+++ app/views/CSC_landing.blade.php
@@ -13,10 +13,25 @@ CSC, LLC.  Landing Page
 <h2>CSC Site Information Page</h2>
 @stop
 @section ('description')
-This will contain information on how to navigate through this application.  Also the RTM.
+<h3>Application navigation:</h3><br />
+<p>This web application is built on Laravel MVC, PHP, HTML, CSS, Toddish RBAC, and Bootstrap.</p>
+There are three defined roles:
+<ul>
+	<li>Admin role</li>
+	<li>Employee role</li>
+	<li>Client role</li>
+</ul>
+<p>If a user signs up for a login account, they are given the default client role.  Depending on the role, a user can only perform
+	the functions identified in the role based matrix diagram.  Authentication and authorization is performed on each function
+	route to determine accessibility.  Depending on the current authenticated user, the "site functions" menu will only show the functions
+	which they can perform.  If a user attempts to call a function route in the URL, the application will determine if they are 
+	authorized or not and will respond accordingly.</p>
+<p>Under the admin role, a user is able to perform all CRUD functionality as in create services, clients, and orders; read services and orders;
+	update services; and delete services.</p>
 @stop
 @section ('content')
 @if(Session::get('flash_message'))
 	<div class='flash-message'>{{ Session::get('flash_message') }}</div>
 @endif
+<img src="images/role_based_matrix.png" alt="RBM Image" />
 @stop
